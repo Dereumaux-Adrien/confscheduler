0c1f4a233b6d6151872876b701b809cfce9f5be1
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-06-11 15:40:14 +0200

    Added a calendar library for use to display confs

--- app/controllers/ConferenceController.scala
+++ app/controllers/ConferenceController.scala
@@ -4,6 +4,10 @@ import play.api._
 import play.api.mvc._
 import com.github.nscala_time.time.Imports._
 
+abstract class CalendarPeriod
+case class Weekly() extends CalendarPeriod
+case class Monthly() extends CalendarPeriod
+
 
 object ConferenceController extends Controller {
   def addConf = Action {
@@ -20,4 +24,12 @@ object ConferenceController extends Controller {
   def listConfs = Action {
     Ok(views.html.index(models.Conference.findAll.sortBy(_.startDate)))
   }
+
+  def calendar(period: CalendarPeriod) = Action {
+    Ok(views.html.weeklyCalendar())
+  }
+
+  def weeklyCalendar = calendar(Weekly())
+
+  def monthlyCalendar = calendar(Monthly())
 }
