2afd6ab1d4886fa82ac6451a42bbd452d3899ee4
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-06-25 17:28:04 +0200

    Added a stub for the creation of new users

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import models.UserRole
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created by thomas on 25/06/14.
+ */
+object UserController extends Controller {
+  def newUser(role: Option[String]) = Action {
+    NotImplemented
+  }
+}
