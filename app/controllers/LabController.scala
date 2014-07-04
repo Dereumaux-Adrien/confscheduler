140c5431688183f5355a6b01953345a5c790a1e0
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-07-04 13:27:24 +0200

    Added the ability to add a new Lab

---
+++ app/controllers/LabController.scala
@@ -0,0 +1,43 @@
+package controllers
+
+import MySecurity.Authentication._
+import controllers.ConferenceController._
+import models.{Administrator, Lab}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc.{Controller, Result}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object LabController extends Controller {
+  case class SimpleLab(acronym: String, name: String)
+
+  val labForm = Form {
+    mapping(
+      "acronym" -> nonEmptyText(1, 100),
+      "name" -> nonEmptyText(1, 3000))(SimpleLab.apply)(SimpleLab.unapply)
+  }
+
+  def newLab = ForcedAuthentication { implicit request => Future {
+    request.user.get.role match {
+      case Administrator => Ok(views.html.labViews.newLab(labForm)(request, request.user.get.role))
+      case _             => Redirect(routes.Application.index()) //TODO: Display a meaningful error message when someone tries to access this page without the rights
+    }
+  }}
+
+  def create = ForcedAuthentication { implicit request => Future {
+    labForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.labViews.newLab(formWithErrors)(request, authenticatedUserRole.get)),
+      newLab         => request.user.get.role match {
+        case Administrator                                     => createLab(newLab)
+        case _                                                 => Redirect(routes.Application.index()) //TODO: Actually display an error message here
+      }
+    )
+  }}
+
+  def createLab(newLab: SimpleLab): Result = {
+    Lab.fromSimpleLab(newLab).get.save //TODO: Actually handle errors during User creation
+    Redirect(routes.Application.index())
+  }
+}
--- app/models/Lab.scala
+++ app/models/Lab.scala
@@ -1,13 +1,26 @@
 package models
 
+import controllers.LabController.SimpleLab
+
 case class Lab (
   id     : Long,
   acronym: String,
   name   : String
-)
+) {
+  def save = Lab.save(this)
+}
 
 object Lab {
   var fixtures = Set(Lab(0, "CIRI", "Centre de truc plutot cools"), Lab(1, "POUET", "Le centre des poetes"))
+  var nextId = 1
 
   def findById(id: Long): Option[Lab] = fixtures.find(_.id == id)
+
+  def fromSimpleLab(lab: SimpleLab): Option[Lab] = {
+    val result = Lab(nextId, lab.acronym, lab.name)
+    nextId += 1
+    Option(result)
+  }
+
+  def save(lab: Lab) = fixtures = fixtures + lab
 }
