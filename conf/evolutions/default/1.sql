2e0467f4a8f100812381320f98d9fbc210259fce
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-07-07 14:09:12 +0200

    Moved the 2.sql evolution to 1.sql, as I want 1.sql to contain the complete base schema

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,4 +1,4 @@
-# Lab schema
+# Creates the initial DB
 # --- !Ups
 
 CREATE TABLE Lab (
@@ -8,6 +8,18 @@ CREATE TABLE Lab (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Speaker (
+    id      bigint NOT NULL AUTO_INCREMENT,
+    title     VARCHAR(20) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName  VARCHAR(255) NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    team      VARCHAR(255) NOT NULL,
+    organisation      VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 # --- !Downs
 
 DROP TABLE Lab;
+DROP TABLE Speaker;
