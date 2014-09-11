3debc85a86e711055b7cd8b9811cff198a447c29
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-09-11 11:07:32 +0200

    Changed the html/JS to add a location adding feature to the UI

--- app/assets/javascripts/conferenceForm.js
+++ app/assets/javascripts/conferenceForm.js
@@ -1,18 +1,5 @@
 $(function() {
-    var speakerSelect = $('#speaker');
-    var newSpeaker = false;
-
-    if(speakerSelect.val() === "-1") {
-        $("#new-speaker").show();
-        newSpeaker = true;
-    }
-
-    var clearNewSpeaker = function() {
-        speakerSelect.find('[value=-1]').remove();
-        $("#new-speaker").hide();
-        newSpeaker = false;
-    };
-
+    // Calendar & Time Picker set-up
     document.getElementById('timezoneOffset').value = new Date().getTimezoneOffset();
 
     $("#datepicker").datepicker({ dateFormat: "yy-mm-dd" });
@@ -31,6 +18,20 @@ $(function() {
         step:15
     });
 
+    // Speaker select & addition set-up
+    var speakerSelect = $('#speaker');
+    var newSpeaker = false;
+
+    if(speakerSelect.val() === "-1") {
+        $("#new-speaker").show();
+        newSpeaker = true;
+    }
+
+    var clearNewSpeaker = function() {
+        speakerSelect.find('[value=-1]').remove();
+        $("#new-speaker").hide();
+        newSpeaker = false;
+    };
     speakerSelect.change(function(){
         if(speakerSelect.val() !== -1) {
             clearNewSpeaker();
@@ -45,6 +46,38 @@ $(function() {
         } else {
             clearNewSpeaker();
             newSpeaker = false;
+        }
+    });
+
+    // Location selection & addition set-up
+    var newLocation = false;
+    var locationSelect = $('#location');
+
+    if(locationSelect.val() === "-1") {
+        $("#new-location").show();
+        newLocation = true;
+    }
+
+    var clearNewLocation = function() {
+        locationSelect.find('[value=-1]').remove();
+        $("#new-location").hide();
+        newLocation = false;
+    };
+
+    locationSelect.change(function(){
+        if(locationSelect.val() !== -1) {
+            clearNewLocation();
+        }
+    });
+
+    $("#toggleNewLocation").click(function() {
+        if(!newLocation) {
+            locationSelect.append('<option value="-1" selected="selected">New Location</option>');
+            $("#new-location").show();
+            newLocation = true;
+        } else {
+            clearNewLocation();
+            newLocation = false;
         }
     });
 });
