53c162f067267525faf3d130dc8cfff9ff992daa
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-10-17 12:04:39 +0200

    Added the legals page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,16 @@
 package controllers
 
-import play.api._
+import MySecurity.Authentication._
+import models.Guest
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
     Redirect(routes.ConferenceController.listUpcomingConfs(None))
+  }
+
+  def legals = MyAuthenticated { implicit request =>
+    Ok(views.html.legals(request, request.user.map(_.role).getOrElse(Guest)))
   }
 }
