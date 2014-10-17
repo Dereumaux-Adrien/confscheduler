addbfdb25e6bf467ec58af27a6c9484a4a2a387f
Thomas Poinsot <thomas.poinsot1@gmail.com>
2014-10-17 11:45:35 +0200

    Now using the Labs' email adresses to send the weekly/monthly reminders

--- app/email/MailingListActor.scala
+++ app/email/MailingListActor.scala
@@ -2,7 +2,7 @@ package email
 
 import akka.actor.{Props, Actor}
 import helpers.DateTimeUtils
-import models.{Conference, User}
+import models.{Lab, Conference, User}
 import org.joda.time.DateTime
 import play.api.libs.concurrent.Akka
 
@@ -20,12 +20,12 @@ class MailingListActor extends Actor {
   val mailer = Akka.system.actorOf(Props[Mailer])
 
   override def receive = {
-    case Mailing(Weekly) => User.listAll.foreach(user => mailer ! SendMail(user.email, "[ConfScheduler] Weekly Conferences Reminder",
-        views.html.email.weeklyConfList.render(Conference.findVisibleByLabBetween(user.lab, DateTime.now, DateTime.now.plusDays(7)), "week").body)
+    case Mailing(Weekly) => Lab.listAll.foreach(lab => mailer ! SendMail(lab.email, "[Bio-Hebdo] Weekly Conferences Reminder",
+        views.html.email.weeklyConfList.render(Conference.findVisibleByLabBetween(lab, DateTime.now, DateTime.now.plusDays(7)), "week").body)
     )
     case Mailing(Monthly) => {
-      User.listAll.foreach(user => mailer ! SendMail(user.email, "[ConfScheduler] Monthly Conferences Reminder",
-        views.html.email.weeklyConfList.render(Conference.findVisibleByLabBetween(user.lab, DateTime.now, DateTime.now.plusMonths(1)), "month").body)
+      Lab.listAll.foreach(lab => mailer ! SendMail(lab.email, "[Bio-Conf] Monthly Conferences Reminder",
+        views.html.email.weeklyConfList.render(Conference.findVisibleByLabBetween(lab, DateTime.now, DateTime.now.plusMonths(1)), "month").body)
       )
       Akka.system.scheduler.scheduleOnce(DateTimeUtils.findNextMonth(1), Akka.system.actorOf(Props[MailingListActor]), Mailing(Monthly))
     }
